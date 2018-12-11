ecbdc33f7e716c5633ae16504f2affca0446b019
Stepan S. Sickar <mkoty@yandex.ru>
2018-12-11 18:26:02 +0700

    init router; added header logo;

--- src/modules/app-routing.module.ts
+++ src/modules/app-routing.module.ts
@@ -1,7 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {StudyListComponent} from '../components/study-list/study-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'studies', component: StudyListComponent},
+  {path: '', redirectTo: '/studies', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
