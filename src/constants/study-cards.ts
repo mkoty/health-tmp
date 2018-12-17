4e5cecea9b7458ca5ce41944fe49ac8b82837f55
Stepan S. Sickar <mkoty@yandex.ru>
2018-12-17 18:28:21 +0700

    edit study-card style

--- src/constants/study-cards.ts
+++ src/constants/study-cards.ts
@@ -3,34 +3,50 @@ import {StudyCard} from '../entities/StudyCard';
 export const studyCards: StudyCard[] = [
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   },
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   },
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   },
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   },
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   },
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   },
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   },
   {
     title: 'card',
+    path: '../card/N1',
+    database: 'Procedure / Premier',
     description: 'description'
   }
 ]
--- src/entities/StudyCard.ts
+++ src/entities/StudyCard.ts
@@ -1,4 +1,6 @@
 export interface StudyCard {
   title: string;
+  path: string;
+  database: string;
   description: string;
 }
