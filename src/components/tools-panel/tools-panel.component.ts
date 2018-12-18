e8e96a3de510e310aaaf761fde279a35c2ee5c12
Stepan S. Sickar <mkoty@yandex.ru>
2018-12-18 18:23:49 +0700

    added menu for studies

--- src/components/tools-panel/tools-panel.component.ts
+++ src/components/tools-panel/tools-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {toolsPanelItems} from '../../constants/tools-menu';
 
 @Component({
   selector: 'app-tools-panel',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tools-panel.component.scss']
 })
 export class ToolsPanelComponent implements OnInit {
+  toolsPanelItems = toolsPanelItems
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
---
+++ src/constants/tools-menu.ts
@@ -0,0 +1,32 @@
+export const toolsPanelItems = [
+  {
+    iconPath: '../assets/tools-panel/available.svg',
+    name: 'Available',
+    isActive: true
+  },
+  {
+    iconPath: '../assets/tools-panel/recent.svg',
+    name: 'Recent',
+    isActive: false
+  },
+  {
+    iconPath: '../assets/tools-panel/cloud.svg',
+    name: 'Cloud',
+    isActive: false
+  },
+  {
+    iconPath: '../assets/tools-panel/shared.svg',
+    name: 'Shared',
+    isActive: false
+  },
+  {
+    iconPath: '../assets/tools-panel/deleted.svg',
+    name: 'Deleted',
+    isActive: false
+  },
+  {
+    iconPath: '../assets/tools-panel/add-folder.svg',
+    name: 'Add Folder',
+    isActive: false
+  }
+];
